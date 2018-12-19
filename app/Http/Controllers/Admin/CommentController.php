6d4d6cc50dd196d04e57f1ef21a8b155d4330be3
boge <438879794@qq.com>
2018-12-20 00:30:02 +0800

    lyb

--- app/Http/Controllers/Admin/CommentController.php
+++ app/Http/Controllers/Admin/CommentController.php
@@ -6,7 +6,6 @@ use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 //引入DB
 use DB;
-use app\Library\helper;
 class CommentController extends Controller
 {
     /**
@@ -32,8 +31,6 @@ class CommentController extends Controller
     public function create()
     {
         //
-        $n = new helper();
-        $n->sendsphone(13202002462);
     }
 
     /**
@@ -121,4 +118,5 @@ class CommentController extends Controller
             echo 0;
         }
     }
+
 }
