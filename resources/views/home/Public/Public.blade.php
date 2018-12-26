9222566924dca5414695c1d38d9f5f51f9a28b80
boge <438879794@qq.com>
2018-12-26 11:15:35 +0800

    lyb

--- resources/views/home/Public/Public.blade.php
+++ resources/views/home/Public/Public.blade.php
@@ -127,7 +127,7 @@
     </div>
     <div class="nav">
       <ul class="navbar">
-        <li><a href="/" data-act="home-click" class="active">首页</a></li>
+        <li><a href="/" data-act="home-click">首页</a></li>
         <li><a href="/films" data-act="movies-click">电影</a></li>
         <li><a href="/films_show_cinema?id=0" data-act="cinemas-click">影院</a></li>
         <li><a href="/List" data-act="board-click">榜单</a></li>
